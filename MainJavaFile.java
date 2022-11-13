b3c92a4edec5fb1e1c0b66668e59a1158ed1c285
Joy Patterson <jp3643@princeton.edu>
2022-11-12 19:03:53 -0500

    fixing the mixup

--- MainJavaFile.java
+++ MainJavaFile.java
@@ -10,8 +10,6 @@ public class MainJavaFile {
     public static void main(String[] args) {
 
         ArrayList<Book> listOfBooks = new ArrayList<Book>(10);
-        /*String review = " ";*/
-        String highestRating = "0";
         BufferedReader file =
                 new BufferedReader(new InputStreamReader(System.in));
         String thisLine = null;
@@ -19,8 +17,7 @@ public class MainJavaFile {
         do {
             try {
                 thisLine = file.readLine();
-            }
-            catch (IOException ex) {
+            } catch (IOException ex) {
                 System.out.println(ex.toString());
                 System.out.println("Could not find file");
             }
@@ -32,11 +29,7 @@ public class MainJavaFile {
                 String rating = fields[8];
                 String pages = fields[11];
                 String date = fields[15];
-                /*String currentReview = fields[19];*/
                 String review = fields[19];
-                /*if (rating.compareTo(highestRating) > 0) {
-                    if (!currentReview.equals(" ")) review = currentReview;
-                }*/
                 Book newBook = new Book(name, author, rating, pages, review, date);
                 listOfBooks.add(newBook);
             }
@@ -48,20 +41,15 @@ public class MainJavaFile {
         Comparator<Book> comparator = Book.byDate();
         listOfBooks.sort(comparator);
 
-        // int size = listOfBooks.size();
-
         List<Book> bookList = listOfBooks.subList(0, 10);
         bookList.sort(comparator);
 
-        for (int a = 0; a < 10; a++) {
-            Book bookDebug = bookList.get(a);
-            System.out.println(bookDebug.name);
-            System.out.println(bookDebug.author);
-            System.out.println(bookDebug.rating);
-            System.out.println(bookDebug.pages);
-            System.out.println(bookDebug.date);
-            System.out.println(bookDebug.review);
-        }
+
+
+        /* ******************************************************/
+
+        TempVisualGeneration menu = new TempVisualGeneration(bookList);
+
     }
 }
 
